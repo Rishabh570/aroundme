14786ce90a253a2fc1ef3a0f5460eb85d66ff5f2
rishabh570 <www.rishabhrawat570@gmail.com>
2018-11-29 15:31:49 +0530

    update readme

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 # The text of the README file
-with open(os.path.join(HERE, "README.md")) as fid:
-    README = fid.read()
-
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name="aroundme",
       version="1.0",
       description="Find interesting places around you.",
-      long_description=README,
+      long_description=long_description,
       long_description_content_type="text/markdown",
       url="https://github.com/rishabh570/what-is-around",
       author="Rishabh Rawat",
